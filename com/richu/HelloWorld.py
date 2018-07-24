15fb539ea98814ebf8d56c92218f406c2da3572a
richukarimattam86 <richu.karimattam@gmail.com>
2018-07-24 00:30:35 -0400

    Load Screen and input method

--- com/richu/HelloWorld.py
+++ com/richu/HelloWorld.py
@@ -31,58 +31,72 @@ from random import shuffle, randrange, choice
 #     img.save('resized.jpg')
 
 
-loadWindow = turtle.Screen()
-loadWindow.setup(500,500)
+def loadScreen():
+    loadWindow = turtle.Screen()
+    loadWindow.setup(500,400,0,0)
+    loadWindow.bgcolor("red")
+    # loadWindow.bgpic("rr.gif")
+
+
+loadScreen()
+
+def askEmail():
+    value = input('Enter your email id:')
+    print("The Email ID %s was entered " % (value))
+    print("The Email ID " + value + " was entered")
+
+askEmail()
+
 #loadWindow.bgpic("roof.jpg")
 #loadWindow.bgcolor("#E1AAE1")
 #loadWindow.bgpic(urllib.request.urlopen)
 #getImage()
 
-taylor = turtle.Turtle()
-taylor.shape('square')
-taylor.color('#FF33FC')
-taylor.speed(0)
-
-print(math.sqrt(2))
-
-print ("Hello World")
-
-colors = ['#ffc412','#f5b426','#eca33a','#e2934d',
-          '#d88361','#cf7275','#c56288','#bb529c',
-          '#b241b0','#a831c4','#9e21d7','#9510eb',
-          '#8b00ff']
-
-taylor.penup()
-#taylor.goto(50, -30)
-taylor.pendown()
-
-for x in range(40):
-    taylor.shape('circle')
-    taylor.pensize(10)
-    taylor.pencolor(colors[x % 6])
-    #taylor.pencolor(choice(colors))
-    taylor.width(x / 90 + 1)
-    taylor.back(x/2)
-    taylor.circle(x*5)
-
-    taylor.left(15)
-#taylor.circle(x)
-
-
-
-taylor.penup
-taylor.goto(0, 0) # this is x y position in frame
-taylor.right(190)
-taylor.speed(4)
-taylor.forward(10)
-taylor.write("Hiya!!", align="left", font=("Arial", 14, "bold"))
-time.sleep(1)
-taylor.color('#c56288')
-taylor.write("Hiya!!", align="left", font=("Arial", 14, "bold"))
-taylor.color('#74c161')
-#taylor.write("I'm Sheetal", align="left", font=("Arial", 14, "bold"))
-#taylor.back(20)
-taylor.setheading(90)
+# taylor = turtle.Turtle()
+# taylor.shape('square')
+# taylor.color('#FF33FC')
+# taylor.speed(0)
+#
+# print(math.sqrt(2))
+#
+# print ("Hello World")
+#
+# colors = ['#ffc412','#f5b426','#eca33a','#e2934d',
+#           '#d88361','#cf7275','#c56288','#bb529c',
+#           '#b241b0','#a831c4','#9e21d7','#9510eb',
+#           '#8b00ff']
+#
+# taylor.penup()
+# #taylor.goto(50, -30)
+# taylor.pendown()
+#
+# for x in range(40):
+#     taylor.shape('circle')
+#     taylor.pensize(10)
+#     taylor.pencolor(colors[x % 6])
+#     #taylor.pencolor(choice(colors))
+#     taylor.width(x / 90 + 1)
+#     taylor.back(x/2)
+#     taylor.circle(x*5)
+#
+#     taylor.left(15)
+# #taylor.circle(x)
+#
+#
+#
+# taylor.penup
+# taylor.goto(0, 0) # this is x y position in frame
+# taylor.right(190)
+# taylor.speed(4)
+# taylor.forward(10)
+# taylor.write("Hiya!!", align="left", font=("Arial", 14, "bold"))
+# time.sleep(1)
+# taylor.color('#c56288')
+# taylor.write("Hiya!!", align="left", font=("Arial", 14, "bold"))
+# taylor.color('#74c161')
+# #taylor.write("I'm Sheetal", align="left", font=("Arial", 14, "bold"))
+# #taylor.back(20)
+# taylor.setheading(90)
 
 
 
